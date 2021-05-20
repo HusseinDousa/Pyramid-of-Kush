bfbcedf9fbd2430c3f348b808eb9155c69d3dc01
Hussein <46466236+ZiX92@users.noreply.github.com>
2021-05-20 12:53:48 -0400

    Instantiating blessings

---
+++ Assets/blessingsSpawn.cs
@@ -0,0 +1,37 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class blessingsSpawn : MonoBehaviour
+{
+
+    public GameObject _blessings;
+    private float _blessingSpawnTime = 3.0f;
+    private bool _stopSpawn = false;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        // testing
+        StartCoroutine(treasureSpawn());
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    IEnumerator treasureSpawn()
+    {
+        yield return new WaitForSeconds(5.0f);
+
+        while(_stopSpawn == false)
+        {
+            Vector3 direction = new Vector3(Random.Range(35.0f, 55.0f), 10.0f, 20.0f);
+            GameObject newBlessing = Instantiate(_blessings, direction, Quaternion.Euler(-90f, 0.0f, 0.0f));
+
+            yield return new WaitForSeconds(_blessingSpawnTime);
+        }
+    }
+}
