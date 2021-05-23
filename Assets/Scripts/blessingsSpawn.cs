5844f5044e08e3ef1029b67d0f3c57607273ca08
Hussein <46466236+ZiX92@users.noreply.github.com>
2021-05-23 16:52:28 -0400

    new changes

--- Assets/Scripts/blessingsSpawn.cs
+++ Assets/Scripts/blessingsSpawn.cs
@@ -19,7 +19,7 @@ public class blessingsSpawn : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
-        
+        // use switch statement to make different collectibles
     }
 
     IEnumerator treasureSpawn()
